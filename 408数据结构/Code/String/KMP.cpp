3a386f18e910feff015ca1d1e711c03ff90dd177
LoafPhilosopher <541101834@qq.com>
2024-04-21 16:35:06 +0800

    树部分除查找外基础内容总结

--- 408数据结构/Code/String/KMP.cpp
+++ 408数据结构/Code/String/KMP.cpp
@@ -2,13 +2,6 @@
 #define int long long
 #define endl "\n"
 using namespace std;
-typedef struct ThreadNode
-{
-    int data;                           // 数据元素
-    struct ThreadNode *lchild, *rchild; // 左,右孩子指针
-    int lTag, rTag;                     // 左右线索标记
-} ThreadNode, *ThreadTree;
-
 signed main()
 {
     ios::sync_with_stdio(false);
