d2933336313ae12a33b61cf5f089de11c9e2bd48
LoafPhilosopher <541101834@qq.com>
2024-05-03 21:02:39 +0800

    树部分易忽视内容补充

--- 408数据结构/Code/String/KMP.cpp
+++ 408数据结构/Code/String/KMP.cpp
@@ -6,6 +6,14 @@ signed main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-
+    cout << log2(256) << endl;
+    cout << ceil( log(51)/log(3)) << endl;
+    cout << floor(log(50)/log(3) + 1) << endl;
+    int count = 0;
+    for (int i = 0; i < 7; i++)
+    {
+        count += pow(2, i);
+        cout << i + 1 << ' ' << count << ' ' << pow(2, i) << endl;
+    }
     return 0;
 }
