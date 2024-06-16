131b0c47896f8c76834cfb94b343ca665b3f1ad0
LoafPhilosopher <541101834@qq.com>
2024-06-17 00:12:41 +0800

    内部排序总结

--- 408数据结构/Code/String/KMP.cpp
+++ 408数据结构/Code/String/KMP.cpp
@@ -1,19 +1,13 @@
-#include <bits/stdc++.h>
-#define int long long
-#define endl "\n"
+#include <iostream>
 using namespace std;
-signed main()
+int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout << log2(256) << endl;
-    cout << ceil( log(51)/log(3)) << endl;
-    cout << floor(log(50)/log(3) + 1) << endl;
-    int count = 0;
-    for (int i = 0; i < 7; i++)
+    int xuan;
+    int tian;
+    int da;
+    while (true)
     {
-        count += pow(2, i);
-        cout << i + 1 << ' ' << count << ' ' << pow(2, i) << endl;
+        cin >> xuan >> tian;
+        cout << 100 - xuan * 5 - tian * 5 << endl;
     }
-    return 0;
 }
